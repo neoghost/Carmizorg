8c888c34c1a7becb320946264e3e856a522fdf50
Alon Kerem <Alon Kerem>
2016-12-08 19:54:05 +0200

    Add project files.

---
+++ Carmizorg/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carmizorg
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                // אתחל מערך של חומוס פול
+                new Carmi_As_A_Service()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
